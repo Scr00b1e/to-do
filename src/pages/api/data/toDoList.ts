fec3fd3ca9e35975bff3a5261df9f8fda4a5ecb6
Scr00b1e <kbolot312@gmail.com>
2023-01-27 14:34:06 +0600

    added apis

---
+++ src/pages/api/data/toDoList.ts
@@ -0,0 +1,5 @@
+export const ToDoList = [
+    { id: '0', name: 'sup bros', isDone: false },
+    { id: '1', name: 'some stuff', isDone: false },
+    { id: '2', name: 'some stuff 2', isDone: false },
+]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,34 @@
 import Add from '@/components/Add'
 import ToDo from '@/components/ToDo'
 import styles from '@/styles/Home.module.css'
+import { GetStaticProps } from 'next'
 
-export default function Home() {
+export default function Home({ todo }) {
   return (
     <div className='container'>
       <Add />
       <ToDo />
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const responce = await fetch(`${process.env.API_HOST}/toDo`)
+    const data = responce.json()
+
+    if (!data) {
+      return {
+        notFound: true
+      }
+    }
+
+    return {
+      props: { todo: data }
+    }
+  } catch {
+    return {
+      props: { todo: null }
+    }
+  }
 }
