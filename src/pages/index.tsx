1aebc3c2f3fed2a90a6e6e0f2d94b04bf3e75bc0
Scr00b1e <kbolot312@gmail.com>
2023-01-27 14:02:58 +0600

    added scss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,19 @@ import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   return (
-    <>
-      sup
-    </>
+    <div className='container'>
+      <div>
+        <input type="text" />
+        <button>Add</button>
+      </div>
+      <div>
+        <ul>
+          <li>Todo 1</li>
+          <li>Todo 2</li>
+          <li>Todo 3</li>
+          <li>Todo 4</li>
+        </ul>
+      </div>
+    </div>
   )
 }
