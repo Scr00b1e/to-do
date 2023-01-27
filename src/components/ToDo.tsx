e1e19e7b487d7d4b68a8e8777fca2310e303691d
Scr00b1e <kbolot312@gmail.com>
2023-01-27 21:05:38 +0600

    fixing bugs

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,16 +1,42 @@
+import { ToDoProps } from '@/types/todo'
+import { GetStaticProps } from 'next'
 import React from 'react'
 
-const ToDo = () => {
+const ToDo: React.FC<ToDoProps> = ({ todos }) => {
+
   return (
     <div>
       <ul>
-        <li>Todo 1</li>
-        <li>Todo 2</li>
-        <li>Todo 3</li>
-        <li>Todo 4</li>
+        {
+          todos &&
+          todos.map((obj: any) => (
+            <li key={obj.id}>{obj.name}</li>
+          ))
+        }
       </ul>
     </div>
   )
 }
 
 export default ToDo
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const res = await fetch(`${process.env.API_HOST}/toDo`)
+    const data = await res.json()
+
+    if (!data) {
+      return {
+        notFound: true
+      }
+    }
+
+    return {
+      props: { todos: data }
+    }
+  } catch {
+    return {
+      props: { todos: null }
+    }
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,12 @@
 import Add from '@/components/Add'
 import ToDo from '@/components/ToDo'
 import styles from '@/styles/Home.module.css'
-import { GetStaticProps } from 'next'
 
-export default function Home({ todo }) {
+export default function Home() {
   return (
     <div className='container'>
       <Add />
-      <ToDo />
+      <ToDo todos={undefined} />
     </div>
   )
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  try {
-    const responce = await fetch(`${process.env.API_HOST}/toDo`)
-    const data = responce.json()
-
-    if (!data) {
-      return {
-        notFound: true
-      }
-    }
-
-    return {
-      props: { todo: data }
-    }
-  } catch {
-    return {
-      props: { todo: null }
-    }
-  }
 }
